b3e181fbf63cb1e34fd4a12641aa562f1d6b8f4f
Micha Kaczmarski <miharu.mk@gmail.com>
2022-12-11 22:26:26 +0100

    6.2 added Calculator class and tests for add and subtract method

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        if(Calculator.add(3,4)==7) {
+            System.out.println("adding test - passed");
+        } else {
+            System.out.println("adding test - failed");
+        }
+
+        if(Calculator.subtract(6,2)==4) {
+            System.out.println("adding test - passed");
+        } else {
+            System.out.println("adding test - failed");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    static public int add(int a, int b){
+        return a+b;
+    }
+
+    static public int subtract(int a, int b){
+        return a-b;
+    }
+}
