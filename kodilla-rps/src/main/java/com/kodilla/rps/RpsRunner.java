d10cabe1763b0ab9f966374fbea8f97bb5eef816
Micha Kaczmarski <miharu.mk@gmail.com>
2022-12-29 15:05:33 +0100

    7.8. "rock paper scissors lizard spock" game v0.1
    TO DO:
    test suite
    finishing selectRoundLimit validation (player still can select 0 rounds)
    level of difficulty
    GUI in Swing

---
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -0,0 +1,10 @@
+package com.kodilla.rps;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.util.Scanner;
+
+public class RpsRunner {
+    public static void main(String[] args) {
+        GameBox.run();
+    }
+}
